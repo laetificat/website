95ec1baa9ed4da75e04475c2971dc53c65f96cc3
Kevin Heruer <k.heruer@plugwise.com>
2015-07-13 17:23:33 +0200

    Busy adding blog functionality

---
+++ src/Laetificat/CommonBundle/Entity/Blog.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Laetficat\CommonBundle\Entity;
+
+class Blog
+{
+
+    private $id;
+
+    private $title;
+
+    private $content;
+
+    private $author;
+
+    private $date;
+
+    public function __construct()
+    {
+
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+}
---
+++ src/Laetificat/CommonBundle/Entity/BlogRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Laetficat\CommonBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class BlogRepository extends EntityRepository
+{
+
+}
