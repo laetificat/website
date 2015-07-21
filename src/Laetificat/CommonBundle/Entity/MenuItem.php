32779e561e6460f74c156d501d9f57123b46588c
Kevin Heruer <k.heruer@plugwise.com>
2015-07-21 16:52:52 +0200

    Added getId()

--- src/Laetificat/CommonBundle/Entity/MenuItem.php
+++ src/Laetificat/CommonBundle/Entity/MenuItem.php
@@ -44,6 +44,11 @@ class MenuItem
         $this->menus = new ArrayCollection();
     }
 
+    public function getId()
+    {
+        return $this->id;
+    }
+
     public function getName()
     {
         return $this->name;
